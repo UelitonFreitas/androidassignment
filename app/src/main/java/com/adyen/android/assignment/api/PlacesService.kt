de919083cc5931c95bd0c4361549cefdc478d23c
uelitonfreitas <freitas.ueliton@gmail.com>
2021-12-08 08:24:46 -0300

    feat: user V3 foursquare API

--- app/src/main/java/com/adyen/android/assignment/api/PlacesService.kt
+++ app/src/main/java/com/adyen/android/assignment/api/PlacesService.kt
@@ -3,10 +3,15 @@ package com.adyen.android.assignment.api
 import com.adyen.android.assignment.BuildConfig
 import com.adyen.android.assignment.api.model.ResponseWrapper
 import com.adyen.android.assignment.api.model.VenueRecommendationsResponse
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.QueryMap
 
 
@@ -16,17 +21,35 @@ interface PlacesService {
      *
      * See [the docs](https://developer.foursquare.com/docs/api/venues/explore)
      */
-    @GET("venues/explore")
+    @GET("/v3/places/search")
     fun getVenueRecommendations(@QueryMap query: Map<String, String>): Call<ResponseWrapper<VenueRecommendationsResponse>>
 
     companion object  {
         private val retrofit by lazy {
             Retrofit.Builder()
                 .baseUrl(BuildConfig.FOURSQUARE_BASE_URL)
+                .client(httpClient)
                 .addConverterFactory(MoshiConverterFactory.create())
                 .build()
         }
 
         val instance: PlacesService by lazy { retrofit.create(PlacesService::class.java) }
+
+        private val httpClient by lazy {
+            OkHttpClient.Builder()
+                .addNetworkInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
+                .addInterceptor(AuthenticationInterceptor())
+                .build()
+        }
+    }
+
+    class AuthenticationInterceptor : Interceptor {
+        override fun intercept(chain: Interceptor.Chain): Response {
+            val request = chain.request().newBuilder().apply {
+                addHeader("Authorization", BuildConfig.FOUR_SQUARE_API_KEY)
+            }.build()
+
+            return chain.proceed(request)
+        }
     }
 }
